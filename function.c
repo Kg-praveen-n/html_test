9f001bb6331e12ff1d944161428183f6b62c8d0a
Kg-praveen-n <praveenpal8193@gmail.com>
2025-10-27 20:10:57 +0530

    Add files via upload

---
+++ function.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void greet(){ 
+    printf("God, Good morning\n");
+    printf("How are\n");
+}
+void print_len(){
+    printf("enter the number");
+    printf("96459612357");
+}
+int main(){
+    printf("I am palshab\n-");
+    greet();
+    printf("I hard work in karhal ");
+
+}
